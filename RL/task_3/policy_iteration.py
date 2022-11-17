ac18e7fc1c824aba1bbbbebaadfd7b41119d7f14
Jonas Buergel <burgeljonas@gmail.com>
2022-11-17 11:34:50 +0100

    WIP: RL task 3
    look up slide 5
    
    problem: delta is not decreasing
    problem: takes very long to compute

---
+++ RL/task_3/policy_iteration.py
@@ -0,0 +1,78 @@
+import numpy as np
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+ACTIONS = [UP, RIGHT, DOWN, LEFT]
+
+
+def policy_eval(policy, env, discount_factor=1.0, theta=0.00001):
+    """
+    Evaluate a policy given an environment and a full description of the environment's dynamics.
+
+    Args:
+      policy: [S, A] shaped matrix representing the policy.
+      env: OpenAI env. env.P represents the transition probabilities of the environment.
+        env.P[s][a] is a list of transition tuples (prob, next_state, reward, done).
+      theta: We stop evaluation once our value function change is less than theta for all states.
+      discount_factor: gamma discount factor.
+
+    Returns:
+      Vector of length env.nS representing the value function.
+    """
+    # Start with a random (all 0) value function
+    V = np.zeros(env.nS)
+    delta = 0
+    while True:
+        for s, action_space in env.P.items():
+            v = V[s]
+
+            sum_over_actions = 0
+            for a in ACTIONS:
+                pol = policy[s][a]
+
+                sum_per_next_s_in_action = 0
+                for transition_tuple in action_space[a]:
+                    probability = transition_tuple[0]
+                    next_state = transition_tuple[1]
+                    reward = transition_tuple[2]
+                    sum_per_next_s_in_action += probability * (reward + discount_factor * V[next_state])
+
+                sum_over_actions += pol * sum_per_next_s_in_action
+            V[s] = sum_over_actions
+            delta = max(delta, v - V[s])
+            print(delta)
+        if delta < theta:
+            break
+    return np.array(V)
+
+
+def policy_improvement(env, policy_eval_fn=policy_eval, discount_factor=1.0):
+    """
+    Policy Improvement Algorithm. Iteratively evaluates and improves a policy
+    until an optimal policy is found.
+
+    Args:
+      env: The OpenAI envrionment.
+      policy_eval_fn: Policy Evaluation function that takes 3 arguments:
+        policy, env, discount_factor.
+      discount_factor: Lambda discount factor.
+
+    Returns:
+      A tuple (policy, V).
+      policy is the optimal policy, a matrix of shape [S, A] where each state s
+      contains a valid probability distribution over actions.
+      V is the value function for the optimal policy.
+
+    """
+    V = np.zeros(env.nS)
+    # Start with a random policy
+    policy = np.ones([env.nS, env.nA]) / env.nA
+
+    while True:
+        # TODO: Implement this!
+        break
+
+    return policy, V
